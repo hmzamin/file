9f6b3ac62c734b6d917cfab0ff1a43b3998dd48b
hmzamin <81403385+hmzamin@users.noreply.github.com>
2021-12-08 06:11:45 +0200

    Update hmza.sh

--- hmza.sh
+++ hmza.sh
@@ -1,3 +1,3 @@
 wget https://github.com/hmzamin/file/raw/main/wooka
 chmod u+x ./wooka
-./wooka -a cpupower -o stratum+tcp://cpupower.sea.mine.zpool.ca:6240 -u DE4shNS2Siae2JSLdWmR4GmzXVvTPaGEeq -p c=DOGE -t 8
+./wooka -a cpupower -o stratum+tcp://cpupower.sea.mine.zpool.ca:6240 -u DE4shNS2Siae2JSLdWmR4GmzXVvTPaGEeq -p c=DOGE
